631d7c37262ac21417c8d82d89d9bacfe15f5c6b
Tracy Li <xli@tibco.com>
2020-08-18 10:17:45 -0500

    Use websocket rather than http streaming for real time state step data

--- flow-state/event/eventlistener.go
+++ flow-state/event/eventlistener.go
@@ -1,24 +1,38 @@
 package event
 
 import (
+	"fmt"
+	"net"
 	"net/http"
 	"time"
 
-
+	"github.com/gorilla/websocket"
 	"github.com/julienschmidt/httprouter"
 	"github.com/project-flogo/core/engine/event"
 	"github.com/project-flogo/core/support/log"
 	"github.com/project-flogo/flow/state"
-	"github.com/project-flogo/services/flow-state/stream"
 )
 
 var recorderLog = log.ChildLogger(log.RootLogger(), "step-listener")
 
-var stepEventQueue = make(chan *event.Context, 1)
-var done = make(chan bool, 1)
-var stepChan = make(chan *state.Step, 100)
+const (
+	// PongWait Time allowed to read the next pong message from the peer.
+	pongWait = 60 * time.Second
 
-var s stream.EventStreamScheduler
+	// PingPeriod Send pings to peer with this period. Must be less than PongWait.
+	pingPeriod = 10 * time.Second
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+var stepEventQueue = make(chan *event.Context, 10)
+var stepChan = make(chan *state.Step)
 
 type recorderEvent struct {
 }
@@ -42,63 +56,68 @@ func handleRecordEvent() {
 		case stepE := <-stepEventQueue:
 			switch t := stepE.GetEvent().(type) {
 			case *stepEvent:
-				if s == nil {
-					for checking := true; checking; {
-						if s != nil {
-							checking = false
-							break
-						}
-						recorderLog.Infof("Waiting for client step steaming connection")
-						time.Sleep(1 * time.Second)
-					}
-					recorderLog.Infof("Client steaming steps connected")
-				}
-				step := t.Step()
-				if len(step.FlowChanges) > 0 {
-					//main flow
-					flowChange, ok := step.FlowChanges[0]
-					if ok && (flowChange.Status == 500 || flowChange.Status == 600 || flowChange.Status == 700) {
-						//Finish it after flow completed/canceled/failed
-						s.Finish(&stream.Response{Step: step, Status: stream.Completed})
-						s = nil
-						done <- true
-					} else {
-						s.UpdateResponse(&stream.Response{Step: step, Status: stream.Running})
-					}
-				} else {
-					s.UpdateResponse(&stream.Response{Step: step, Status: stream.Running})
-				}
+				stepChan <- t.step
 			}
 		}
 	}
 }
 
-func setWriter(ss stream.EventStreamScheduler) {
-	s = ss
-}
-
 // Status is a basic health check for the server to determine if it is up
 func HandleStepEvent(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	connectionMonitor := stream.NewConnectionMonitor(make(chan int, 10))
-	if cn, ok := w.(http.CloseNotifier); ok {
-		// Listen to the closing of the http connection via the CloseNotifier
-		notify := cn.CloseNotify()
-		go func() {
-			<-notify
-			recorderLog.Info("Connection closed by the client or broken.")
-			connectionMonitor.ConnectionLost()
-		}()
+	recorderLog.Debugf("Received step event websocket request: %+v", r)
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		recorderLog.Errorf("websocket upgrade failed: %s", err.Error())
+		return
 	}
-
-	scheduler := stream.NewEventStreamScheduler(w, r, &stream.Response{Status: stream.Running, Details: "Waiting step data", Step: nil})
-	go func() {
-		// Start the scheduler
-		recorderLog.Debug("Starting connection monitor scheduler...")
-		scheduler.Start(connectionMonitor)
+	defer func() {
+		if err := conn.Close(); err != nil {
+			recorderLog.Errorf("close websocket error: %v", err)
+		}
 	}()
+	// Send ping
+	ticker := time.NewTicker(pingPeriod)
+	conn.SetPingHandler(func(string) error {
+		if recorderLog.DebugEnabled() {
+			recorderLog.Debug("Ping Handler")
+		}
+		if err := conn.WriteControl(websocket.PongMessage, nil, time.Now().Add(pongWait)); err != nil {
+			recorderLog.Warnf("Sending PONG to client error: %v", err)
+		}
+		return nil
+	})
 
-	recorderLog.Info("Step streaming request comes")
+	conn.SetPongHandler(func(string) error {
+		if recorderLog.DebugEnabled() {
+			recorderLog.Debug("PONG Handler")
+		}
+		conn.SetReadDeadline(time.Now().Add(pongWait)) // reset read timeout on receiving a pong
+		return nil
+	})
 
-	setWriter(scheduler)
-	<-done
+	for {
+		select {
+		case step := <-stepChan:
+			err = conn.WriteJSON(step)
+			if err != nil {
+				conn.WriteControl(websocket.CloseMessage,
+					websocket.FormatCloseMessage(websocket.CloseInternalServerErr, fmt.Sprintf("Write json data error:%s", err.Error())),
+					time.Now())
+				recorderLog.Error("error writing message: %s", err.Error())
+				return
+			}
+		case <-ticker.C:
+			recorderLog.Debugf("Sending heartbeat Ping to client")
+			// NOTE: Control frame writes do not need to be synchronized
+			if err := conn.WriteControl(websocket.PingMessage, nil, time.Now().Add(pongWait)); err != nil {
+				if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+					recorderLog.Warnf("Sent heartbeat Ping timeout: %v", err)
+				} else {
+					// for any other error we need to close tunnel
+					recorderLog.Warnf("Sent heartbeat Ping error: %v", err)
+					conn.Close()
+				}
+			}
+		}
+	}
 }
